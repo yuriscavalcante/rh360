47ec4eac4ddfd9ffcd9013046dd672124a21fdbc
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2026-01-13 14:57:23 -0300

    fix: cors

--- src/main/java/com/rh360/rh360/config/WebConfig.java
+++ src/main/java/com/rh360/rh360/config/WebConfig.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -19,5 +21,14 @@ public class WebConfig {
         registration.addUrlPatterns("/api/*");
         registration.setOrder(1);
         return registration;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // ou "*"
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
