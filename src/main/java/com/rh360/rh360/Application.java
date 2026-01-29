e4b053fbf3656dcba8bc638fbfc9ae5f5bcafb3b
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2026-01-29 12:26:08 -0300

    fix

--- src/main/java/com/rh360/rh360/Application.java
+++ src/main/java/com/rh360/rh360/Application.java
@@ -2,8 +2,10 @@ package com.rh360.rh360;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.rh360.rh360.repository")
 public class Application {
 
 	public static void main(String[] args) {
