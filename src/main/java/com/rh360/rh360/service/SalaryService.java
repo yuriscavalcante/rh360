dfc308fdb5ddbe84d307f8f9779bd69aac7c7788
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2026-01-29 12:37:58 -0300

    fix

--- src/main/java/com/rh360/rh360/service/SalaryService.java
+++ src/main/java/com/rh360/rh360/service/SalaryService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,6 +32,7 @@ public class SalaryService {
     private final R2StorageService r2StorageService;
     private final RealTimePublisher realTimePublisher;
 
+    @Autowired
     public SalaryService(
             SalaryRepository repository,
             SalaryAttachmentRepository attachmentRepository,
